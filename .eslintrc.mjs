f8650b094c737acb0830fca9b74230682b3eb5fd
MadaraUchiha-314 <rohithr31+github@gmail.com>
2024-02-18 08:51:42 -0800

    fix: initial setup

---
+++ .eslintrc.mjs
@@ -0,0 +1,23 @@
+export default {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs,mjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
